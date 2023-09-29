f0c676452068da629a7e51c37fc568924ffa5529
Wilhelm Mozes <wilhelmmozes@gmail.com>
2023-09-29 10:52:53 +0700

    Refactor: move route from server to other file

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 require('dotenv').config()
+const taskRouter = require('./src/route/task.route')
+// const taskController = require('./src/controller/task.controller')
+const apiBaseUrl = '/api';
+const app = express()
+const port = process.env.PORT || 3000
 
-const taskController = require('./src/controller/task.controller')
+app.use(apiBaseUrl+'/tasks', taskRouter)
+app.use(bodyParser.json())
 
-const app = express();
-const port = process.env.PORT || 3000;
+// app.get('/api/tasks', (req, res) => {
+//     taskController.getTasks().then(data => res.json(data));
+// });
 
-app.use(bodyParser.json());
+// app.post('/api/tasks', (req, res) => {
+//     console.log(req.body);
+//     taskController.createTask(req.body.task).then(data => res.json(data));
+// });
 
-app.get('/api/tasks', (req, res) => {
-    taskController.getTasks().then(data => res.json(data));
-});
+// app.put('/api/tasks', (req, res) => {
+//     taskController.updateTask(req.body.task).then(data => res.json(data));
+// });
 
-app.post('/api/tasks', (req, res) => {
-    console.log(req.body);
-    taskController.createTask(req.body.task).then(data => res.json(data));
-});
-
-app.put('/api/tasks', (req, res) => {
-    taskController.updateTask(req.body.task).then(data => res.json(data));
-});
-
-app.delete('/api/tasks/:id', (req, res) => {
-    taskController.deleteTask(req.params.id).then(data => res.json(data));
-});
+// app.delete('/api/tasks/:id', (req, res) => {
+//     taskController.deleteTask(req.params.id).then(data => res.json(data));
+// });
 
 app.get('/', (req, res) => {
     res.send(`<h1>API Works !!!</h1>`)
